18787045a57c5931611bcf62a408c6fb75832025
Muniox <muniox20@gmail.com>
2024-04-14 13:21:39 +0200

    Using HostListener to listen to Host Events

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,9 +1,10 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit  {
+
 
   constructor(
     private elementRef: ElementRef,
@@ -11,9 +12,24 @@ export class BetterHighlightDirective implements OnInit  {
   ) {}
 
   ngOnInit() {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    this.renderer.setStyle(this.elementRef.nativeElement, 'cursor', 'pointer');
   }
 
   // jest to lepsze podejście, ponieważ angular może działać jako serwis worker, przez co nie będziemy mieli dostępu
-  // do elementu dom, dzięki renderer możemy zmieć style bez dostępu do DOM
+  // do elementu dom, dzięki renderer możemy, zmieć style bez dostępu do "DOM"
+
+  @HostListener('mouseenter') mouseover(
+    // eventData: MouseEvent
+  ) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'white');
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseleave') mouseout(
+    // eventData: MouseEvent
+  ) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'black');
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+  }
 }
